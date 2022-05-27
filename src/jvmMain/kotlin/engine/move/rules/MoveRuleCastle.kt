6ad297409d5b6e3a25d9537cc85e10e3e9594ffa
Jeff Palmer <jeff@jeffpalm.dev>
2022-05-27 01:40:48 -0400

    Perft Position 4 depth 2 success

--- src/jvmMain/kotlin/engine/move/rules/MoveRuleCastle.kt
+++ src/jvmMain/kotlin/engine/move/rules/MoveRuleCastle.kt
@@ -13,20 +13,25 @@ class MoveRuleCastle : IMoveRule {
 
     override suspend fun run(ctx: MoveGenCtx) {
         val (board, turn) = ctx.data
+        val enemyAttacks = board.allAttackTargets(turn.inv())
         when (turn) {
             Color.WHITE -> {
-                if (board.rayMoves(Sq.h1, Direction.W, Color.WHITE) == 0x60UL) {
+                val kingSidePath = board.rayMoves(Sq.h1, Direction.W, Color.WHITE)
+                val queenSidePath = board.rayMoves(Sq.b1, Direction.E, Color.WHITE)
+                if (kingSidePath == 0x60UL && kingSidePath.and(enemyAttacks) == 0UL) {
                     ctx.addMove(PseudoMove(Square.e1, Square.g1, Piece.wKing))
                 }
-                if (board.rayMoves(Sq.a1, Direction.E, Color.WHITE) == 0xEUL) {
+                if (queenSidePath == 0xCUL && queenSidePath.and(enemyAttacks) == 0UL) {
                     ctx.addMove(PseudoMove(Square.e1, Square.c1, Piece.wKing))
                 }
             }
             Color.BLACK -> {
-                if (board.rayMoves(Sq.h8, Direction.W, Color.BLACK) == 0x6000000000000000UL) {
+                val kingSidePath = board.rayMoves(Sq.h8, Direction.W, Color.BLACK)
+                val queenSidePath = board.rayMoves(Sq.b8, Direction.E, Color.BLACK)
+                if (kingSidePath == 0x6000000000000000UL && kingSidePath.and(enemyAttacks) == 0UL) {
                     ctx.addMove(PseudoMove(Square.e8, Square.g8, Piece.bKing))
                 }
-                if (board.rayMoves(Sq.a8, Direction.E, Color.BLACK) == 0xE00000000000000UL) {
+                if (queenSidePath == 0xC00000000000000UL && queenSidePath.and(enemyAttacks) == 0UL) {
                     ctx.addMove(PseudoMove(Square.e8, Square.c8, Piece.bKing))
                 }
             }
